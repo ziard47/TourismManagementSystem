483ee6bf66c5316f83c70a5a5888df2ed6133b32
ziard <mohomedziard5@gmail.com>
2023-06-03 18:49:30 +0530

    1st Commit

---
+++ src/main/java/com/abctours/tourSystem/TourSystemApplication.java
@@ -0,0 +1,13 @@
+package com.abctours.tourSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abctours/tourSystem/TourSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abctours.tourSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
